e64da99a25c6078b543feaf48140cdc29083ab48
mitrovicbojan <79321143+mitrovicbojan@users.noreply.github.com>
2021-11-17 11:54:07 +0200

    commit

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 var i = 0;
 var images = [];
-var time = 3000;
+var time = 5000;
 
 images[0] = '/img/photo-1.jpg';
 images[1] = '/img/photo-2.jpg';
 images[2] = '/img/photo-3.jpg';
 
 function changeImg() {
-    document.slide.src = images[i];
-
-    if(i<images.length - 1){
-        i++;
-    } else {
-        i = 0;
-    }
+    document.slide.src = images[i];    
 
     setTimeout("changeImg()", time);
-
+    return i<images.length - 1 ? i++ : i = 0;
 }
 
 window.onload = changeImg;
